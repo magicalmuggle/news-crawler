c46b86fd22007f6186b4eb3eade5c45f6d3ffcc1
magicalmuggle <magicalmuggle@foxmail.com>
2022-04-25 15:05:07 +0800

    Upgrade Apache httpclient to 5.1.3

--- src/main/java/com/github/magicalmuggle/Main.java
+++ src/main/java/com/github/magicalmuggle/Main.java
@@ -1,20 +1,21 @@
 package com.github.magicalmuggle;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
             HttpGet httpGet = new HttpGet("https://sina.cn/");
             try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
-                System.out.println(response.getStatusLine());
+                System.out.println(response.getCode() + " " + response.getReasonPhrase());
                 HttpEntity entity = response.getEntity();
                 System.out.println(EntityUtils.toString(entity));
             }
