690c96d87ce9aa7b39fdd3eaa439af523584f803
magicalmuggle <magicalmuggle@foxmail.com>
2022-05-11 22:01:44 +0800

    Use MyBatis

---
+++ src/main/java/com/github/magicalmuggle/News.java
@@ -0,0 +1,37 @@
+package com.github.magicalmuggle;
+
+public class News {
+    private String url;
+    private String title;
+    private String content;
+
+    public News(String url, String title, String content) {
+        this.url = url;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
