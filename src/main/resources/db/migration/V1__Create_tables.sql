f07bce2428070d350a5b28e5e06f5167d2f05a57
magicalmuggle <magicalmuggle@foxmail.com>
2022-05-12 18:09:51 +0800

    设置数据库 table 的 charset 为 utf8mb4

--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -6,13 +6,13 @@ create table news
     url         varchar(4096),
     created_at  timestamp default current_timestamp,
     modified_at timestamp default current_timestamp on update current_timestamp
-);
+) default charset=utf8mb4;
 
 create table links_to_be_processed
 (
     link varchar(4096)
-);
+) default charset=utf8mb4;
 create table links_already_processed
 (
     link varchar(4096)
-);
+) default charset=utf8mb4;
