3705f0bbb37cddf7b0ff70218388f86c951ed8f1
magicalmuggle <magicalmuggle@foxmail.com>
2022-05-28 17:28:00 +0800

    Use Elasticsearch (#6)
    1. 使用 ElasticsearchDataGenerator 来生成数据
    2. 使用 ElasticsearchEngine 作为命令行搜索工具

---
+++ src/main/java/com/github/magicalmuggle/ElasticsearchDataGenerator.java
@@ -0,0 +1,69 @@
+package com.github.magicalmuggle;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.BulkRequest;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class ElasticsearchDataGenerator {
+    public static void main(String[] args) {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        List<News> currentNewsList = getNewsFromMySQL(sqlSessionFactory);
+        currentNewsList.forEach(news -> News.cutNewsContent(news, 10)); // 裁剪插入新闻的内容长度，从而节省硬盘空间
+
+        int numOfThreads = 16;
+        for (int i = 0; i < numOfThreads; i++) {
+            new Thread(() -> {
+                try {
+                    writeSingleThread(currentNewsList);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+        }
+    }
+
+    private static void writeSingleThread(List<News> currentNewsList) throws IOException {
+        ElasticsearchClient client = ElasticsearchUtil.getElasticsearchClient();
+        try {
+            // 单线程写入 1000 * 1000 = 1_000_000 条数据
+            for (int i = 0; i < 1000; i++) {
+                BulkRequest.Builder BulkRequestBuilder = new BulkRequest.Builder();
+
+                for (News news : currentNewsList) {
+                    BulkRequestBuilder.operations(op -> op.index(idx -> idx.index("news").document(news)));
+                }
+                client.bulk(BulkRequestBuilder.build());
+
+                System.out.println("Current thread: " + Thread.currentThread().getName() + " finishes " + i);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            client._transport().close();
+        }
+    }
+
+    private static List<News> getNewsFromMySQL(SqlSessionFactory sqlSessionFactory) {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            MockMapper mapper = session.getMapper(MockMapper.class);
+            return mapper.selectNews();
+        }
+    }
+
+    private static SqlSessionFactory getSqlSessionFactory() {
+        try {
+            String resource = "db/mybatis/config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            return new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
